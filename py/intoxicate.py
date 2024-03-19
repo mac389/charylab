f76c8c16ed17f5ff5cdd442bb5bd9b4109362ab7
Michael Chary <mac389@gmail.com>
2024-03-19 13:04:46 -0300

    weekly update

--- py/intoxicate.py
+++ py/intoxicate.py
@@ -1,37 +1,50 @@
 import pandas as pd
-from bokeh.sampledata.iris import flowers
+import numpy as np
+import os
 
+from bokeh.sampledata.iris import flowers
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource, HoverTool
-
 from bokeh.embed import components, json_item
 
-colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
-flowers['colors'] = [colormap[x] for x in flowers['species']]
+from bokeh.io import show
+from bokeh.models import CheckboxGroup, CustomJS
 
-hover = HoverTool(tooltips=[
-    ("Sepal length", "@sepal_length"),
-    ("Sepal width", "@sepal_width"),
-    ("Petal length", "@petal_length"),
-    ("Species", "@species")
-    ])
+DATA_PATH = os.path.join('..', '_data', 'intoxicate')
 
-p = figure(title = "Iris Morphology", height=500, width=500, tools=[hover, "pan,reset,wheel_zoom"])
+df = pd.read_csv(os.path.join(DATA_PATH, 'intoxicate.patient_registry.stable.csv'))
 
-p.xaxis.axis_label = 'Petal Length'
-p.yaxis.axis_label = 'Petal Width'
+p = figure(title = "Explore Intoxicate", 
+           height=500, width=500,
+           tools=["pan,reset,wheel_zoom"])
 
-p.circle('petal_length', 'petal_width', color='colors', 
-         fill_alpha=0.2, size=10, source=ColumnDataSource(flowers))
+p.xaxis.axis_label = 'Risk Score'
+p.yaxis.axis_label = 'No. of Patients'
+p.vspan(x=[6],line_width=[2], line_color="red", line_dash="dashed")
+# Histogram
+bins = np.linspace(0, 60, num=20)
+hist, edges = np.histogram(df['risk'], density=False, bins=bins)
 
-output_file('../_includes/custom/intoxicate_interactive_plot.html')
+LABELS = ["Option 1", "Option 2", "Option 3"]
 
-# Extract <script> and <div> elements to embed content in MD files. 
+checkbox_group = CheckboxGroup(labels=LABELS, active=[0, 1])
+checkbox_group.js_on_change('active', CustomJS(code="""
+    console.log('checkbox_group: active=' + this.active, this.toString())
+"""))
+p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+         fill_color="black", line_color="white")
 
 script, div = components(p)
+checks = components(checkbox_group)
 
-# Save the script and div elements to a file.
 with open('../_includes/custom/intoxicate_embed_plot.html', 'w') as file:
-    file.write(div)
-    file.write('\n\n')
+    script, div = components(p)
     file.write(script)
+    file.write('\n')
+    file.write(div)
+    file.write('\n')
+
+    script, div = components(checkbox_group)
+    file.write(script)
+    file.write('\n')
+    file.write(div)
